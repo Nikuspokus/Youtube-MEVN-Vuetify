6934c0aa26fce3fba592f723281eb2107f74b348
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-31 23:26:12 +0200

    MEVN - Youtube clone - vuetify - bug connect mongoose

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+
+const DBconnection = require('./config/db.js')
 
 dotenv.config({path: './config/.env'})
 
+DBconnection()
+
 const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use((req, res, next) => {
   res.json({title: 'hello world'})
---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const DBconnection = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }).catch(err => {
+    console.log(`For some reasons we couldn't connect to the DB`.red, err);
+  })
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.black.bgYellow);
+}
+
+module.exports = DBconnection
